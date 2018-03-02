e517b14e96348042585897cef002dd38a689cdda
SToyokazu <kyukyusya945@docomo.ne.jp>
2018-03-02 23:40:51 +0900

    add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+yes | sudo apt-get update
+yes | sudo apt-get upgrade
+
+mv $HOME/デスクトップ $HOME/Desktop
+mv $HOME/ダウンロード $HOME/Downloads
+mv $HOME/テンプレート $HOME/Templates
+mv $HOME/公開 $HOME/Share
+mv $HOME/ドキュメント $HOME/Documents
+mv $HOME/音楽 $HOME/Music
+mv $HOME/画像 $HOME/Pictures
+mv $HOME/ビデオ $HOME/Videos
