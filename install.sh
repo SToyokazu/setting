cae301671da879a1a47a5ac887f4d214aa092d56
SToyokazu <kyukyusya945@docomo.ne.jp>
2018-03-02 23:52:27 +0900

    mv コマンドの画像と音楽をピクチャとミュージックに修正

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ mv $HOME/ダウンロード $HOME/Downloads
 mv $HOME/テンプレート $HOME/Templates
 mv $HOME/公開 $HOME/Share
 mv $HOME/ドキュメント $HOME/Documents
-mv $HOME/音楽 $HOME/Music
-mv $HOME/画像 $HOME/Pictures
+mv $HOME/ミュージック $HOME/Music
+mv $HOME/ピクチャ $HOME/Pictures
 mv $HOME/ビデオ $HOME/Videos
